6fd9b901c0e58a2d88b18017dd82fc922d39321a
Daniel Coady <me@pondo.dev>
2024-02-03 10:37:20 +1100

    add raylib windowing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+#include "raylib.h"
 #include "helpers.h"
 
+#define WINDOW_WIDTH 512
+#define WINDOW_HEIGHT 512
+
 int main(void) {
-    printf("TODO!\n");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "c ecs");
+    while (! WindowShouldClose())
+    {
+        // update state
+        // TODO
+
+        // drawing
+        BeginDrawing();
+        {
+            ClearBackground(RAYWHITE);
+            DrawRectangle(0, 0, 50, 50, PINK);
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
